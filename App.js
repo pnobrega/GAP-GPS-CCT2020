09f71fcf83b2623d0b3ded2c37c453421f3d3ca5
Leandro F <61284549+LeandroFrazao@users.noreply.github.com>
2020-09-21 21:04:09 +0100

    commit

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Gps} from './'
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Gps />
     </View>
   );
 }
@@ -17,5 +20,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
