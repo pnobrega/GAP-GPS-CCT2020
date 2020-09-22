2e0a0f4edc56c787dd612a78604fc7e645cdc390
Leandro Frazao <leandrofrazao@hotmail.com>
2020-09-22 18:39:08 +0100

    commit

--- gps.js
+++ gps.js
@@ -1,43 +1,31 @@
-import React, { Component } from 'react';
-import { Alert, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import Style from './Style/Style';
+import React, { Component } from "react";
+import { Alert, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default class Gps extends Component {
-	state = {
-		location: null
-	};
+  state = {
+    location: null,
+  };
 
-	findCoordinates = () => {
-		navigator.geolocation.getCurrentPosition(
-			position => {
-				const location = JSON.stringify(position);
+  findCoordinates = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const location = JSON.stringify(position);
 
-				this.setState({ location });
-			},
-			error => Alert.alert(error.message),
-			{ enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-		);
-	};
+        this.setState({ location });
+      },
+      (error) => Alert.alert(error.message),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+  };
 
-	render() {
-		return (
-			<View  >
-				<TouchableOpacity onPress={this.findCoordinates} style={styles.container}>
-					<Text >Find My Coords kct!</Text>
-					<Text>Location: {this.state.location}</Text>
-				</TouchableOpacity>
-			</View>
-		);
-	}
+  render() {
+    return (
+      <View>
+        <TouchableOpacity onPress={this.findCoordinates}>
+          <Text>Find My Coords?</Text>
+          <Text>Location: {this.state.location}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
-const styles = StyleSheet.create({
-	container: {
-	  flex: 1,
-	  backgroundColor: "lightgray",
-	  borderWidth:1,
-	  borderColor: '#55',
-	  width:150,
-	  alignItems: 'center',
-	  justifyContent: 'center',
-	}
-})
