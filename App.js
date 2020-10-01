38b2959042f35d18e3a2d67d60a90373275875cb
Leandro Frazao <leandrofrazao@hotmail.com>
2020-10-01 02:27:41 +0100

    added colors

--- App.js
+++ App.js
@@ -1,31 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 import Gps from './Gps';
 
 export default function MainScreen() {
-  
-  
-  
   return (
     <View style={styles.container}>
         <StatusBar style={{color: "black"}} 
-            backgroundColor= '#FFB146'
+            backgroundColor= '#169B62'
             color= 'red'
+            barStyle= 'dark-content'
         />
-        <View>
+        <View style={styles.container_logo} >
           <Image style={styles.logo} source={{ uri: 'https://www.cct.ie/wp-content/themes/hdcct/img/atoms/logo.jpg'}} /> 
         </View>
         
         <View style={styles.body}>
           <Text style={styles.title}>Mobile Project: GPS</Text>
-            
           <Gps />
         </View>
-        <View style={styles.foot.container}>
-            <Text style={styles.text}>Developers:</Text>
-            <Text style={styles.text}>Alessandro, Junior, Leandro e Paulo.</Text>
-            <Text style={styles.text}>©</Text>
+            <View style={styles.foot_container} >
+            <Text style={styles.foot_text}>Developers:</Text>
+            <Text style={styles.foot_text}>Alessandro, Junior, Leandro & Paulo.</Text>
+            <Text style={styles.foot_text}>©</Text>
         </View>
     </View>
   );
@@ -33,36 +30,44 @@ export default function MainScreen() {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
 
   },
+  container_logo:{
+    //flex : 2,
+  },
   logo: {
     marginTop:50, 
-    width: 300,
-    height: 60,
+    resizeMode: 'stretch',
+    width: 250,
+    height: 80,
   },
   body: {
-    flex:0,
-    marginTop:40,
+    //flex: 2,
     alignItems: 'center',
     justifyContent: 'flex-end',
   },
   title:{
     fontSize:30,
-    alignItems: 'baseline',
+    //alignItems: 'baseline',
   },
-  foot:{
-    container: {
-      justifyContent: 'flex-end',
-      alignItems: '',
-      
-    },
-    text:{
-      fontSize:10,
+  foot_container: {
+    //flex: -1,
+    marginTop:'10%',
+    //marginBottom:10,
+    width: '100%',
+    //flexDirection:'column' ,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: '#FF8200',
     
-    }, 
-  },
+    },
+  foot_text:{
+    fontSize:12,
+  
+    },
   
 });
  
